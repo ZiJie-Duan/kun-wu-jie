24c7be79c063d10c90c1f1869d1640e2e240f708
zddua <zddua@student.unimelb.edu.au>
2024-09-27 03:01:08 +1000

    update engine parts

--- src/engine/Sprite.java
+++ src/engine/Sprite.java
@@ -8,13 +8,18 @@ public abstract class Sprite<SI, SO> extends Element<SI, SO> {
   private Boundary bd;
   private Image img;
   private Boolean enableBoundaryCheck = false;
-  private Boolean visible = true;
 
-  public void moveX(int x) {
-    this.loc.moveX(x);
+  public Sprite(Loc loc, Boundary bd, Image img) {
+    this.loc = loc;
+    this.bd = bd;
+    this.img = img;
   }
 
-  public void moveY(int y) {
-
+  public Sprite(Loc loc, Boundary bd, Image img, Boolean enableBoundaryCheck) {
+    this.loc = loc;
+    this.bd = bd;
+    this.img = img;
+    this.enableBoundaryCheck = enableBoundaryCheck;
   }
+
 }
