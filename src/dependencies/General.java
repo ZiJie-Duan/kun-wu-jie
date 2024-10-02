fbe9b7de2453b6f83e7098ae0a7faca405f751e9
zddua <zddua@student.unimelb.edu.au>
2024-10-02 22:53:53 +1000

    add user info page fix text error

--- src/dependencies/General.java
+++ src/dependencies/General.java
@@ -4,13 +4,13 @@ import bagel.Font;
 
 public class General {
 
-    public static int getMidXlocForScreenWithFixWide(int wide, int screenWide){
-        return (int) (screenWide - wide)/2 ;
-    }
+  public static int getMidXlocForScreenWithFixWide(int textWide, int screenWide) {
+    return (int) (screenWide - textWide) / 2;
+  }
 
-    public static int getStringWideWithFixFront(String front, int size, String text){
-        Font ft = new Font(front, size);
-        return (int)ft.getWidth(text);
-    }
+  public static int getStringWideWithFixFront(String front, int size, String text) {
+    Font ft = new Font(front, size);
+    return (int) ft.getWidth(text);
+  }
 
 }
